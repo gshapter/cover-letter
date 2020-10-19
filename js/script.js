3dd1af41d78670ded5d08631732854d0e88dd1b0
gshapter <72624855+gshapter@users.noreply.github.com>
2020-10-19 22:42:42 +0100

    menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-
 function revealMenu() {
   document.getElementById("menu").className = "menu";
+  const menuItems = document.querySelectorAll(".menu__item a")
+  menuItems.forEach(item => {
+    if (item.pathname === document.location.pathname) {
+      item.classList.add("menu__item--activePage")
+    }
+  });
+}
+
+function closeMenu() {
+  document.getElementById("menu").classList.add("menu--hidden");
 }
